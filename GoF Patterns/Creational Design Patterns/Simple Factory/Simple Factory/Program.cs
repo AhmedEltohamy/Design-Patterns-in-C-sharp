bad0726cbc284ae554c49468dd9d90814305a102
AhmedEltohamy <ahmedtohamy759@gmail.com>
2023-04-25 08:19:47 +0200

    Complete Simple Factory Pattern

---
+++ GoF Patterns/Creational Design Patterns/Simple Factory/Simple Factory/Program.cs
@@ -0,0 +1,19 @@
+﻿using Simple_Factory;
+
+try
+{
+    CreditCardFactory.GetCreditCard("Titanium").Details();
+    Console.WriteLine();
+
+    CreditCardFactory.GetCreditCard("Platinum").Details();
+    Console.WriteLine();
+
+    CreditCardFactory.GetCreditCard("MoneyBack").Details();
+    Console.WriteLine();
+
+    CreditCardFactory.GetCreditCard("Prime").Details();
+}
+catch (Exception)
+{
+    Console.WriteLine("\n!!! Un Supported Card Type\n");
+}
