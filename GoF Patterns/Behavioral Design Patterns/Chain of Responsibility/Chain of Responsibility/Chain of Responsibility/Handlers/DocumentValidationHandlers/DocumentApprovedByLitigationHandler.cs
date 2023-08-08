ed26d8a8ef1107d1d7c74e42e530ab7167f011f6
AhmedEltohamy <ahmedtohamy759@gmail.com>
2023-08-08 15:09:14 +0300

    Complete Chain of Responsibility Pattern

---
+++ GoF Patterns/Behavioral Design Patterns/Chain of Responsibility/Chain of Responsibility/Chain of Responsibility/Handlers/DocumentValidationHandlers/DocumentApprovedByLitigationHandler.cs
@@ -0,0 +1,23 @@
+﻿using Chain_of_Responsibility.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chain_of_Responsibility.Handlers.DocumentValidationHandlers;
+
+internal class DocumentApprovedByLitigationHandler : IHandler<Document>
+{
+    private IHandler<Document>? _next;
+
+    public void Handle(Document request)
+    {
+        if (!request.ApprovedByLitigation)
+            throw new ValidationException("Document must be approved by litigation.");
+
+        _next?.Handle(request);
+    }
+
+    public IHandler<Document> SetNext(IHandler<Document> nextHandler)
+    {
+        _next = nextHandler;
+        return nextHandler;
+    }
+}
