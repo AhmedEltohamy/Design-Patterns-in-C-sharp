21888ca4d459e66c799e35f1cbfe2fb665e608ad
AhmedEltohamy <ahmedtohamy759@gmail.com>
2023-08-10 19:07:40 +0300

    Complete Interpreter Pattern

---
+++ GoF Patterns/Behavioral Design Patterns/Interpreter/Interpreter/Interpreter/MathContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Interpreter;
+
+internal class MathContext
+{
+    private readonly Dictionary<string, int> _variables;
+
+    public MathContext() => _variables = new Dictionary<string, int>();
+
+    public void SetVariable(string name, int value) => _variables[name] = value;
+
+    public int GetVariable(string name) => _variables[name];
+}
---
+++ GoF Patterns/Behavioral Design Patterns/Interpreter/Interpreter/Interpreter/Program.cs
@@ -0,0 +1,24 @@
+﻿using Interpreter;
+using Interpreter.Expressions;
+
+var context = new MathContext();
+context.SetVariable("x", 2);
+context.SetVariable("y", 3);
+
+// 10 + 20 * (x + y)
+var expression = new AddExpression(
+    new NumberExpression(10),
+    new MultiplyExpression(
+        new NumberExpression(20),
+        new AddExpression(
+            new NumberExpression(context.GetVariable("x")),
+            new NumberExpression(context.GetVariable("y"))
+        )
+    )
+);
+
+var result = expression.Interpret(context);
+
+Console.WriteLine($"Result: {result}");
+
+Console.ReadKey();
