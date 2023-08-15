2896bed2e616b06bda76efe8cafb3d3c72c0ccb0
AhmedEltohamy <ahmedtohamy759@gmail.com>
2023-08-16 01:31:01 +0300

    Complete Observer Pattern

---
+++ GoF Patterns/Behavioral Design Patterns/Observer/Observer/Observer/Observers/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Observer.Observers;
+
+internal class Customer : IProductObserver
+{
+    public string UserName { get; private set; }
+
+    public Customer(string userName)
+    {
+        if (userName is null)
+            throw new ArgumentNullException(nameof(userName));
+
+        UserName = userName;
+    }
+
+    public void OnNewProductAvailable(string productName) => 
+        Console.WriteLine($"Customer with username: {UserName} has been notified that product {productName} is now avaliable at the shop");
+}
---
+++ GoF Patterns/Behavioral Design Patterns/Observer/Observer/Observer/Subjects/Shop.cs
@@ -0,0 +1,40 @@
+﻿using Observer.Observers;
+
+namespace Observer.Subjects;
+
+internal class Shop : ISubject
+{
+    private List<IProductObserver> _observers = new List<IProductObserver>();
+    
+    private List<string> _products = new List<string>();
+
+    public void AddNewProduct(string productName)
+    {
+        if (string.IsNullOrWhiteSpace(productName))
+            throw new ArgumentNullException(nameof(productName));
+
+        if (_products.Any(p => p == productName))
+            throw new InvalidOperationException($"Product with name: {productName} is already exists");
+
+        _products.Add(productName);
+        NotifyObservers(productName);
+    }
+
+    public void RegisterObserver(IProductObserver observer)
+    {
+        if (observer is null)
+            throw new ArgumentNullException(nameof(observer));
+
+        _observers.Add(observer);
+    }
+
+    public void RemoveObserver(IProductObserver observer)
+    {
+        if (observer is null)
+            throw new ArgumentNullException(nameof(observer));
+
+        _observers.Remove(observer);
+    }
+
+    public void NotifyObservers(string newProduct) => _observers.ForEach(o => o.OnNewProductAvailable(newProduct));
+}
