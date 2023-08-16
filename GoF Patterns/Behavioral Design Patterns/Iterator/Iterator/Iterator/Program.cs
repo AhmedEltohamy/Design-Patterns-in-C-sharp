67ae60417968347c162f26ac30f70040833b1bd9
AhmedEltohamy <ahmedtohamy759@gmail.com>
2023-08-16 22:42:55 +0300

    Complete Iterator Pattern

---
+++ GoF Patterns/Behavioral Design Patterns/Iterator/Iterator/Iterator/Program.cs
@@ -0,0 +1,22 @@
+﻿using Iterator;
+
+var people = new People()
+{
+    new Person("Mohamed"),
+    new Person("Yasser"),
+    new Person("Ahmed"),
+};
+
+var enumerator = people.GetEnumerator();
+while (enumerator.MoveNext())
+{
+    var person = enumerator.Current;
+    Console.WriteLine(person.Name);
+}
+
+Console.WriteLine("********************************************");
+
+foreach (var person in people)
+    Console.WriteLine(person.Name);
+
+Console.ReadKey();
